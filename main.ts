4c897b51e3847dc618e77ba7f76e6ec130ade857
une-personne <103508249+une-personne@users.noreply.github.com>
2022-04-19 14:19:46 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,49 @@
+basic.forever(function () {
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
+        . . . . .
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        # . # . #
+        . . # . .
+        . . # . .
+        . . . . .
+        . . # . .
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . . . .
+        . . # . .
+        . # # # .
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        . . # . .
+        . . . . .
+        . . # . .
+        . # # # .
+        # . # . #
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # # # .
+        # . # . #
+        . . # . .
+        `)
+})
